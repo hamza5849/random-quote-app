18cc08534eaeec5e9a7e4fcc02e1f3f3b52dd157
Hamza Asghar <ha7098171@gmail.com>
2025-09-18 22:40:30 -0700

    Rebuild with correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/random-quote-app/", // ensure this is just '/' for Netlify
-});
+  base: "/random-quote-app/", // 👈 this MUST match your repo name
+})
